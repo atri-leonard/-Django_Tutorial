88a856b13de21acaf83ddf01fab779cb6cf0ef18
atri-leonard <yang313@atrihub.io>
2018-01-03 12:01:23 -0800

    feat: Chapter 3 of tutorial
    Now chapter 3 of Django tutorial is complete
    
    ICTRAINING-115

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,21 +1,23 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render, get_object_or_404
 from .models import Question
+
 
 # Create your views here.
 # A real simple index view
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context,request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 # Returns a page with question id
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # Get question via question ID
+    question = get_object_or_404(Question, pk=question_id)
+    # Raise http 404 when there's no question
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 # Returns a page with question result of corresponding id
