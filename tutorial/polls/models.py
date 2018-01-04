b41f12aeae34d6da10ff37cb0480b15ffffcc8ed
atri-leonard <yang313@atrihub.io>
2018-01-03 16:54:32 -0800

    feat: Chapter7
    Django tutorial chapter 7 completed
    
    ICTRAINING-119

--- tutorial/polls/models.py
+++ tutorial/polls/models.py
@@ -18,6 +18,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 # A choice model that entails text of choice, votes count and foreign key that links to a question
